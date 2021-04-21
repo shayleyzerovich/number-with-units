b723f12d7d4fbb2ca4d6a82f0a63c6398b43df56
shayleyzerovich <58327880+shayleyzerovich@users.noreply.github.com>
2021-04-21 14:52:01 +0300

    Create NumberWithUnits.hpp

---
+++ NumberWithUnits.hpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+using namespace std;
+namespace ariel{
+        class NumberWithUnits{
+        
+        private:
+             double number;
+             string type;
+        
+        
+        public:
+            NumberWithUnits(double num,string unit)
+            {
+                number=num;
+                type=unit;
+            }
+            ~NumberWithUnits(){};
+
+            static void read_units(ifstream& units_file);
+
+            // +, +=, +, -, -=, -
+            friend NumberWithUnits& operator+(const NumberWithUnits& num1, const NumberWithUnits& num2);
+            friend NumberWithUnits& operator+=(NumberWithUnits& num1, const NumberWithUnits& num2);
+            friend NumberWithUnits& operator+(const NumberWithUnits& num1);
+            friend NumberWithUnits& operator-(const NumberWithUnits& num1, const NumberWithUnits& num2);
+            friend NumberWithUnits& operator-=(NumberWithUnits& num1, const NumberWithUnits& num2);
+            friend NumberWithUnits& operator-(const NumberWithUnits& num1);
+          
+            // >, >=, <, <=, ==, !=
+            friend bool operator>(const NumberWithUnits& num1, const NumberWithUnits& num2);
+            friend bool operator>=(const NumberWithUnits& num1, const NumberWithUnits& num2);
+            friend bool operator<(const NumberWithUnits& num1, const NumberWithUnits& num2);
+            friend bool operator<=(const NumberWithUnits& num1, const NumberWithUnits& num2);
+            friend bool operator==(const NumberWithUnits& num1, const NumberWithUnits& num2);
+            friend bool operator!=(const NumberWithUnits& num1, const NumberWithUnits& num2);
+          
+            // ++ after, ++ before, -- after, -- before
+            friend NumberWithUnits& operator++( NumberWithUnits& num1);     
+            friend NumberWithUnits& operator++( NumberWithUnits& num1, int);  
+            friend NumberWithUnits& operator--( NumberWithUnits& num1);             
+            friend NumberWithUnits& operator--( NumberWithUnits& num1, int);   
+          
+            // *, *, *=, *=
+            friend NumberWithUnits& operator*(double num1,  NumberWithUnits& num2);
+            friend NumberWithUnits& operator*(  NumberWithUnits& num2,double num1);
+            friend NumberWithUnits& operator*=( double num1,NumberWithUnits  &num2);     
+            friend NumberWithUnits& operator*=(  NumberWithUnits& num2, double num1);
+           
+          
+            friend std::istream& operator>> (std::istream& input , NumberWithUnits& t);
+            friend std::ostream& operator<< (std::ostream& output, const NumberWithUnits& t);
+        
+
+
+    };    
+    
+}
